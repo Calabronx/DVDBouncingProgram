bbe0f058c5119a8f327991629fab9d2f7000d729
unknown <sebicala@hotmail.com>
2025-01-20 19:24:40 -0300

    first imgui config

--- DVDBouncingSimulator.cpp
+++ DVDBouncingSimulator.cpp
@@ -1,6 +1,10 @@
 /**
 	Programmed by Sebastian Calabro
 **/
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
